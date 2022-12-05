59adf0c93c6671fceede6e0a4e341476a387c6ca
Jay <95107672+Jay-zar@users.noreply.github.com>
2022-12-05 18:30:34 -0500

    Created Profile model and added associations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_201651) do
+ActiveRecord::Schema.define(version: 2022_12_05_232432) do
 
   create_table "forums", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2022_12_05_201651) do
     t.integer "forum_id", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "twitter"
+    t.string "github"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_12_05_201651) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
